239d08e5aecac1f72b391e5a1ca88855da7f2803
pavan kalyan <pavankalyanmrpk45@gmail.com>
2024-05-22 23:11:13 +0530

    hierarchical inhertance

---
+++ src/com/tcs/javaoops/inheritance/HierarchicalInhertance.java
@@ -0,0 +1,24 @@
+package com.tcs.javaoops.inheritance;
+class School {
+	public void schoolName() {
+		System.out.println("IT company");
+	}
+}
+class PythonTeacher {
+	public void python() {
+		System.out.println("python");
+	}
+}
+class JavaTeacher {
+	public void java() {
+		System.out.println("java");
+	}
+}
+class JavaScriptTeacher {
+	public void javaScript() {
+		System.out.println("java script");
+	}
+}
+public class HierarchicalInhertance {
+
+}
